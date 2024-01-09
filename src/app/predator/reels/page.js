06abb06a836fa3b04c9395a87e79c1a1dea41728
msselizabeth <lizagotovitryby@gmail.com>
2024-01-09 16:55:26 +0200

    change

--- src/app/predator/reels/page.js
+++ src/app/predator/reels/page.js
@@ -1,36 +1,36 @@
-import Image from "next/image";
-import Link from "next/link";
+// import Image from "next/image";
+// import Link from "next/link";
 
-async function getData() {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/reels/predator`
-  );
-  // The return value is *not* serialized
+// async function getData() {
+//   const res = await fetch(
+//     `${process.env.NEXT_PUBLIC_API_URL}/api/reels/predator`
+//   );
+//   // The return value is *not* serialized
 
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
+//   if (!res.ok) {
+//     // This will activate the closest `error.js` Error Boundary
+//     throw new Error("Failed to fetch data");
+//   }
 
-  return res.json();
-}
+//   return res.json();
+// }
 
-export default async function PredatorReels() {
+// export default async function PredatorReels() {
 
-  const data = await getData();
+//   const data = await getData();
 
 
-  return <div className="container">
-    <ul>
-      {data.map(reel => {
-        return (
-          <li key={reel._id}>
-            <Image src={reel.img[0]} alt={reel.alt} width={200} height={200} />
-            <Link href={`/predator/reels/${reel._id}`}>{`Котушка ${reel.brand} ${reel.series} `}
-            </Link>
-          </li>
-        );
-      })}
-    </ul>
-  </div>;
-}
+//   return <div className="container">
+//     <ul>
+//       {data.map(reel => {
+//         return (
+//           <li key={reel._id}>
+//             <Image src={reel.img[0]} alt={reel.alt} width={200} height={200} />
+//             <Link href={`/predator/reels/${reel._id}`}>{`Котушка ${reel.brand} ${reel.series} `}
+//             </Link>
+//           </li>
+//         );
+//       })}
+//     </ul>
+//   </div>;
+// }
