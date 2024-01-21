6baf8c74c16c6b0fa0ed7dc2605400a6013fddfb
msselizabeth <lizagotovitryby@gmail.com>
2024-01-21 12:19:36 +0200

    fix

--- data/prymanky-ta-prykormky.js
+++ data/prymanky-ta-prykormky.js
@@ -1,52 +1,52 @@
 export const prymankyPrykormkyMenu = [
-  {
-    id: 1,
-    path: "/prymanky-ta-prykormky/bleshni",
-    name: "Блешні",
-  },
-  {
-    id: 2,
-    path: "/prymanky-ta-prykormky/balansyry",
-    name: "Балансири",
-  },
-  {
-    id: 3,
-    path: "/prymanky-ta-prykormky/voblery",
-    name: "Воблери",
-  },
+  // {
+  //   id: 1,
+  //   path: "/prymanky-ta-prykormky/bleshni",
+  //   name: "Блешні",
+  // },
+  // {
+  //   id: 2,
+  //   path: "/prymanky-ta-prykormky/balansyry",
+  //   name: "Балансири",
+  // },
+  // {
+  //   id: 3,
+  //   path: "/prymanky-ta-prykormky/voblery",
+  //   name: "Воблери",
+  // },
   {
     id: 4,
     path: "/prymanky-ta-prykormky/sylikon",
     name: "Силікон",
   },
-  {
-    id: 5,
-    path: "/prymanky-ta-prykormky/attraktanty-ta-likvidy",
-    name: "Ліквіди",
-  },
-  {
-    id: 6,
-    path: "/prymanky-ta-prykormky/nasadky",
-    name: "Насадки",
-  },
-  {
-    id: 7,
-    path: "/prymanky-ta-prykormky/spreyi",
-    name: "Спреї",
-  },
-  {
-    id: 8,
-    path: "/prymanky-ta-prykormky/dipy",
-    name: "Діпи",
-  },
-  {
-    id: 9,
-    path: "/prymanky-ta-prykormky/dobavky",
-    name: "Добавки",
-  },
-  {
-    id: 10,
-    path: "/prymanky-ta-prykormky/prykormka",
-    name: "Прикормка",
-  },
+  // {
+  //   id: 5,
+  //   path: "/prymanky-ta-prykormky/attraktanty-ta-likvidy",
+  //   name: "Ліквіди",
+  // },
+  // {
+  //   id: 6,
+  //   path: "/prymanky-ta-prykormky/nasadky",
+  //   name: "Насадки",
+  // },
+  // {
+  //   id: 7,
+  //   path: "/prymanky-ta-prykormky/spreyi",
+  //   name: "Спреї",
+  // },
+  // {
+  //   id: 8,
+  //   path: "/prymanky-ta-prykormky/dipy",
+  //   name: "Діпи",
+  // },
+  // {
+  //   id: 9,
+  //   path: "/prymanky-ta-prykormky/dobavky",
+  //   name: "Добавки",
+  // },
+  // {
+  //   id: 10,
+  //   path: "/prymanky-ta-prykormky/prykormka",
+  //   name: "Прикормка",
+  // },
 ];
--- data/rybalski-snasti.js
+++ data/rybalski-snasti.js
@@ -4,29 +4,29 @@ export const rybalskiSnastiMenu = [
     path: "/rybalski-snasti/vudylyshcha",
     name: "Вудилища",
   },
-  {
-    id: 2,
-    path: "/rybalski-snasti/katushky",
-    name: "Котушки",
-  },
-  {
-    id: 3,
-    path: "/rybalski-snasti/shnury-ta-volosin",
-    name: "Шнури та волосінь",
-  },
-  {
-    id: 4,
-    path: "/rybalski-snasti/sadky-i-pidsaky",
-    name: "Садки, підсаки та кукани",
-  },
-  {
-    id: 5,
-    path: "/rybalski-snasti/syhnalizatory",
-    name: "Сигналізатори",
-  },
-  {
-    id: 6,
-    path: "/rybalski-snasti/rodpody-stiyky-ta-pidstavky",
-    name: "Род-поди, стійки та підставки",
-  },
+  // {
+  //   id: 2,
+  //   path: "/rybalski-snasti/katushky",
+  //   name: "Котушки",
+  // },
+  // {
+  //   id: 3,
+  //   path: "/rybalski-snasti/shnury-ta-volosin",
+  //   name: "Шнури та волосінь",
+  // },
+  // {
+  //   id: 4,
+  //   path: "/rybalski-snasti/sadky-i-pidsaky",
+  //   name: "Садки, підсаки та кукани",
+  // },
+  // {
+  //   id: 5,
+  //   path: "/rybalski-snasti/syhnalizatory",
+  //   name: "Сигналізатори",
+  // },
+  // {
+  //   id: 6,
+  //   path: "/rybalski-snasti/rodpody-stiyky-ta-pidstavky",
+  //   name: "Род-поди, стійки та підставки",
+  // },
 ];
