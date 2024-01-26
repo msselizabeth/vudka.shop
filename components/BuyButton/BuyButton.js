f853dd95be56c6e9be8c9d3b319361adec66adfa
msselizabeth <lizagotovitryby@gmail.com>
2024-01-26 14:13:16 +0200

    fix btn

--- components/BuyButton/BuyButton.js
+++ components/BuyButton/BuyButton.js
@@ -1,5 +1,5 @@
 "use client"
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styles from "./BuyButton.module.css";
 
 const BuyButton = ({ productId, productName, productPrice, productImg }) => {
